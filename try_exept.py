754750ab3ad24d84837c36e8b49cf4fe08c6e93a
Astro247 <manuelsarti40@gmail.com>
2025-01-24 02:24:41 +0100

    File Modified

--- try_exept.py
+++ try_exept.py
@@ -1,6 +1,9 @@
 #L'istruzione try analizza il codice che lo contiene, se quest'ultimo ritorna un'errore allora viene eseguito il codice all'interno dell'istruzione except
 
+print("Primo Print:")
+
 var = input("Inserisci un numero: ")
+
 try:
     var = int(var)
 except:
@@ -13,6 +16,8 @@ except:
             continue
 
 #Un singolo try possono essere associati più except e ad ogni except può essere specificato il tipo di errore che deve analizzare in caso si riscontri nel try
+
+print("Secondo Print:")
 
 var = input("Inserisci un numero diverso da zero: ")
 
@@ -31,4 +36,25 @@ except:
         except ValueError:
             var = input("Non hai inserito un numero, ripetere: ")
             continue
+
+#L'istruzione "finally" esegue un blocco di codice indipendentemente dal fatto che il blocco di codice nell'except venga letto oppure no.
+
+print("Terzo Print:")
+
+var = "Stringa"
+try:
+    intero = var + 1
+except TypeError:
+    print("Sono entrato nell'except")
+finally:
+    print("Avanti (parte 1)")
+
+var = 5
+try:
+    intero = var + 1
+except TypeError:
+    print("Sono entrato nell'except")
+finally:
+    print("Avanti (parte 2)") 
+
             
